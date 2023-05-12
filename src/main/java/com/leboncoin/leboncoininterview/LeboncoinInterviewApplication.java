5cacde10e2cb14c9bdd5d0928cfa8a652e4afbbd
Arthur Joly <jolyarth@amazon.com>
2023-05-12 19:33:22 +0100

    initial commit

---
+++ src/main/java/com/leboncoin/leboncoininterview/LeboncoinInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.leboncoin.leboncoininterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeboncoinInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeboncoinInterviewApplication.class, args);
+    }
+
+}
